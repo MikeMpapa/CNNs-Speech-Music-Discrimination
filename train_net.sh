299bf440afe4503ef3f31cc27ef81c627ce43cee
Michalis Papakostas <mikepapakostas@gmail.com>
2016-09-26 12:00:28 +0300

    Update train_net.sh

--- train_net.sh
+++ train_net.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#TOOLS=../lisa-caffe-public/build/tools
 TOOLS=../caffe/build/tools
 
 GLOG_logtostderr=1 $TOOLS/caffe train                -solver SM_imagenet_2000_noaug_solver.prototxt -weights caffe_imagenet_hyb2_wr_rc_solver_sqrt_iter_310000
